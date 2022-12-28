8edf16ca78e6c3ca908a5e381143e20a833c01c4
Gustavo <gust4v0309@gmail.com>
2022-12-28 20:25:39 -0300

    feat: att main

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
+import mysql.connector
 from flask import Flask, make_response, jsonify, request
 from bd import Carros
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='MainUser',
+    password='MainPassword',
+    database='Pycodebr',
+)
+
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
+
+    my_cursor = mydb.cursor()
+    my_cursor.execute('SELECT * FROM carros')
+    meus_carros = my_cursor.fetchall()
+
+    carros = list()
+    for carro in meus_carros:
+        carros.append(
+            {
+                'id': carro[0],
+                'marca': carro[1],
+                'modelo': carro[2],
+                'ano': carro[3]
+            }
+        )
+
     return make_response(
         jsonify(
             message='Lista de carros',
-            data=Carros
+            data=carros
         )
     )
 
@@ -17,7 +42,14 @@ def get_carros():
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json
-    Carros.append(carro)
+
+    my_cursor = mydb.cursor()
+
+    sql = f"INSERT INTO carros (marca, modelo, ano) VALUE('{carro['marca']}','{carro['modelo']}',{carro['ano']})"
+
+    my_cursor.execute(sql)
+    mydb.commit()
+
     return make_response(
         jsonify(
             message='Adicionando carro',
